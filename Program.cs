12e70266409234fac72232dfbdbfcdd7ca383595
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 22:11:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio_6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a, b, calculo;
+		
+			Console.WriteLine("Digite um valo em reais: ");
+			a = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Digite a cotação dolar: ");
+			b = Convert.ToDouble(Console.ReadLine());
+
+			calculo = a * b;
+
+			Console.WriteLine("O valor convertido é: " + calculo);
+
+			Console.ReadKey();
+			
+		}
+	}
+}
